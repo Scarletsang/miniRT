28a88f7794ca44d080e3ba1e6e52aeffb680da18
Anthony Tsang <anthonytsang0226@gmail.com>
2023-08-26 15:47:08 +0200

    chore(spec): tests for parser for camera

--- spec/tests/parser_test.hpp
+++ spec/tests/parser_test.hpp
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   module_test.hpp                                    :+:      :+:    :+:   */
+/*   parser_test.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 20:00:58 by htsang            #+#    #+#             */
-/*   Updated: 2023/07/02 15:13:38 by htsang           ###   ########.fr       */
+/*   Updated: 2023/08/26 04:52:32 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRINGBUILDER_TEST_HPP
-# define STRINGBUILDER_TEST_HPP
+#ifndef MRT_PARSER_TEST_HPP
+# define MRT_PARSER_TEST_HPP
 
 # include <gtest/gtest.h>
 
 extern "C"
 {
-	#include "LIBFT/stringbuilder.h"
+  #include "LIBFT/vector.h"
+  #include "MINIRT/unit/range.h"
+  #include "MINIRT/unit/vec3.h"
+  #include "MINIRT/scene.h"
+  #include "MINIRT/scene/s_camera.h"
+  #include "MINIRT/scene/s_game_object.h"
+  #include "MINIRT/scene/s_light.h"
+	#include "MINIRT/parser/general_parser.h"
+  #include "MINIRT/parser/scene_parser.h"
 }
 
 class ModuleTest : public ::testing::TestWithParam<const char *>
