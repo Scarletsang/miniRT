a72592f8481b08506019c573ba2420a114c86630
Anthony Tsang <anthonytsang0226@gmail.com>
2023-09-11 05:35:42 +0200

    feat(camera): Add fov

--- src/world/camera/viewport.c
+++ src/world/camera/viewport.c
@@ -6,25 +6,26 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 20:05:38 by htsang            #+#    #+#             */
-/*   Updated: 2023/09/02 14:57:25 by htsang           ###   ########.fr       */
+/*   Updated: 2023/09/11 05:29:48 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MINIRT/world/camera.h"
+#include <math.h>
 
 /**
  * @brief Calculate the lower left corner of the viewport
  * @details equation = origin - horizontal / 2 - vertical / 2 - focal_length
 */
 static t_mrt_vec3	mrt_viewport_top_left_corner(\
-t_mrt_point3d origin, t_mrt_vec3 horizontal, t_mrt_vec3 vertical, \
-double focal_length)
+struct s_mrt_scene_camera *scene_camera, t_mrt_vec3 horizontal, \
+t_mrt_vec3 vertical, double focal_length)
 {
 	t_mrt_vec3	top_left_corner;
 
 	top_left_corner = vec3_subtract(\
-		origin, \
-		vec3(0.0, 0.0, focal_length));
+		scene_camera->origin, \
+		vec3_smultiply(scene_camera->orientation, focal_length));
 	top_left_corner = vec3_subtract(\
 		top_left_corner, \
 		vec3_sdivide(horizontal, 2.0));
@@ -34,23 +35,38 @@ double focal_length)
 	return (top_left_corner);
 }
 
+double	mrt_degrees_to_radians(double degree)
+{
+	return (degree * (M_PI / 180.0));
+}
+
+double	mrt_focal_length_height(double fov)
+{
+	return (2.0 * tan(mrt_degrees_to_radians(fov) / 2.0));
+}
+
 struct s_mrt_viewport	mrt_viewport(struct s_mrt_scene_camera *scene_camera, \
 struct s_mrt_image screen, double focal_length)
 {
 	t_mrt_direction3d	horizontal;
 	t_mrt_direction3d	vertical;
 	t_mrt_dimension		dimension;
+	t_mrt_vec3			u;
+	t_mrt_vec3			v;
 
-	dimension.y = 2.0;
+	dimension.y = 2.0 * focal_length * \
+		mrt_focal_length_height(scene_camera->fov);
 	dimension.x = dimension.y * screen.aspect_ratio;
-	horizontal = vec3(dimension.x, 0.0, 0.0);
-	vertical = vec3(0.0, -dimension.y, 0.0);
+	u = vec3_normalize(vec3_cross(vec3(0, 1, 0), scene_camera->orientation));
+	v = vec3_cross(scene_camera->orientation, u);
+	horizontal = vec3_smultiply(u, dimension.x);
+	vertical = vec3_smultiply(vec3_negate(v), dimension.y);
 	return ((struct s_mrt_viewport){
 		.dimension = dimension,
 		.horizontal = horizontal,
 		.vertical = vertical,
 		.top_left_corner = mrt_viewport_top_left_corner(
-			scene_camera->origin
+			scene_camera
 			, horizontal
 			, vertical
 			, focal_length),
